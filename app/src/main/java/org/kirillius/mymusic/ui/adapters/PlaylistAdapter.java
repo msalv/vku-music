78f93beca9e463f83b128131de632e39ad8f6378
Kirill Bykov <bikovgta13@gmail.com>
2016-02-21 14:36:13 +0300

    Introduce onPlayButtonClicked listener

--- app/src/main/java/org/kirillius/mymusic/ui/adapters/PlaylistAdapter.java
+++ app/src/main/java/org/kirillius/mymusic/ui/adapters/PlaylistAdapter.java
@@ -20,6 +20,7 @@ public class PlaylistAdapter extends EndlessScrollAdapter<VKApiAudio> {
     private StringBuilder mStringBuilder;
 
     private OnItemClickListener onActionButtonClicked;
+    private OnItemClickListener onPlayButtonClicked;
     public int currentItemId = -1;
 
     public PlaylistAdapter() {
@@ -30,6 +31,10 @@ public class PlaylistAdapter extends EndlessScrollAdapter<VKApiAudio> {
         this.onActionButtonClicked = onActionButtonClicked;
     }
 
+    public void setOnPlayButtonClicked(OnItemClickListener onPlayButtonClicked) {
+        this.onPlayButtonClicked = onPlayButtonClicked;
+    }
+
     @Override
     public EndlessScrollAdapter.Holder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
@@ -38,7 +43,7 @@ public class PlaylistAdapter extends EndlessScrollAdapter<VKApiAudio> {
         switch (viewType) {
             case ITEM_VIEW_TYPE:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.audio_item, parent, false);
-                holder = new ItemHolder(view, this.clickListener, this.onActionButtonClicked);
+                holder = new ItemHolder(view, this.clickListener, this.onActionButtonClicked, this.onPlayButtonClicked);
                 break;
 
             default:
@@ -80,9 +85,10 @@ public class PlaylistAdapter extends EndlessScrollAdapter<VKApiAudio> {
         public TextView duration;
 
         public View buttons_container;
+        public ImageButton play;
         public ImageButton action;
 
-        public ItemHolder(View itemView, OnItemClickListener clickListener, final OnItemClickListener onAction) {
+        public ItemHolder(View itemView, OnItemClickListener clickListener, final OnItemClickListener onAction, final OnItemClickListener onPlay) {
             super(itemView, clickListener);
 
             artist = (TextView) itemView.findViewById(R.id.artist);
@@ -98,6 +104,17 @@ public class PlaylistAdapter extends EndlessScrollAdapter<VKApiAudio> {
                     @Override
                     public void onClick(View v) {
                         onAction.onItemClick(v, getLayoutPosition());
+                    }
+                });
+            }
+
+            play = (ImageButton)itemView.findViewById(R.id.play);
+
+            if ( onPlay != null ) {
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onPlay.onItemClick(v, getLayoutPosition());
                     }
                 });
             }
