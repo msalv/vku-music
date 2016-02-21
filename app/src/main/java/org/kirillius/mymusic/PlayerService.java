9d9ebe44ab6e208b61c9ab7aafc3fe29b23f10fe
Kirill Bykov <bikovgta13@gmail.com>
2016-02-21 19:40:35 +0300

    Play music

--- app/src/main/java/org/kirillius/mymusic/PlayerService.java
+++ app/src/main/java/org/kirillius/mymusic/PlayerService.java
@@ -7,20 +7,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.vk.sdk.api.model.VKApiAudio;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Created by Kirill on 21.02.2016.
  */
-public class PlayerService extends Service {
+public class PlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
     public static final String EXTRA_TRACKS = "ExtraTracks";
     public static final String EXTRA_POSITION = "ExtraPosition";
@@ -36,8 +40,8 @@ public class PlayerService extends Service {
     private ArrayList<VKApiAudio> mTracks = new ArrayList<>();
     private int mCurrentPosition = 0;
     private int mTotalCount = 0;
-    private boolean mIsPlaying = false;
 
+    private MediaPlayer mMediaPlayer;
     private PlayerBroadcastReceiver mReceiver;
 
     @Override
@@ -59,6 +63,10 @@ public class PlayerService extends Service {
 
         registerReceiver(mReceiver, intentFilter);
 
+        if ( !isPlaying() ) {
+            playTrack();
+        }
+
         // show notification
         updateNotification();
 
@@ -69,6 +77,42 @@ public class PlayerService extends Service {
     public void onCreate() {
         super.onCreate();
         mReceiver = new PlayerBroadcastReceiver();
+    }
+
+    private void playTrack() {
+
+        VKApiAudio track = mTracks.get(mCurrentPosition);
+
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(track.url);
+        }
+        catch (IOException e) {
+            Log.e("mymusic", e.getMessage());
+        }
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnPreparedListener(this);
+        mMediaPlayer.prepareAsync();
+
+        mMediaPlayer.setOnCompletionListener(this);
+    }
+
+    private boolean isPlaying() {
+        if ( mMediaPlayer != null ) {
+            return mMediaPlayer.isPlaying();
+        }
+        return false;
+    }
+
+    private void togglePlayer() {
+        if ( mMediaPlayer != null ) {
+            if ( isPlaying() ) {
+                mMediaPlayer.pause();
+            }
+            else {
+                mMediaPlayer.start();
+            }
+        }
     }
 
     /**
@@ -82,7 +126,7 @@ public class PlayerService extends Service {
         contentView.setTextViewText(R.id.artist, track.artist);
         contentView.setTextViewText(R.id.track, track.title);
 
-        int resId = mIsPlaying ? R.drawable.ic_pause_black_24dp : R.drawable.ic_play_arrow_black_24dp;
+        int resId = isPlaying() ? R.drawable.ic_pause_black_24dp : R.drawable.ic_play_arrow_black_24dp;
         contentView.setImageViewResource(R.id.play, resId);
 
         contentView.setOnClickPendingIntent(R.id.prev, PendingIntent.getBroadcast(this, 0, new Intent(ACTION_PREV), 0));
@@ -103,16 +147,55 @@ public class PlayerService extends Service {
         startForeground(NOTIFICATION_ID, notification);
     }
 
+    private void releaseMediaPlayer() {
+        if ( mMediaPlayer != null ) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = null;
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mReceiver);
+        releaseMediaPlayer();
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+        updateNotification();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        playNext();
+    }
+
+    private void playPrev() {
+        mCurrentPosition = --mCurrentPosition < 0 ? mTracks.size() - 1 : mCurrentPosition;
+        if ( mMediaPlayer != null ) {
+            mMediaPlayer.stop();
+        }
+
+        playTrack();
+        updateNotification();
+    }
+
+    private void playNext() {
+        mCurrentPosition = ++mCurrentPosition % mTracks.size();
+        if ( mMediaPlayer != null ) {
+            mMediaPlayer.stop();
+        }
+
+        playTrack();
+        updateNotification();
     }
 
     private class PlayerBroadcastReceiver extends BroadcastReceiver {
@@ -122,19 +205,18 @@ public class PlayerService extends Service {
             String action = intent.getAction();
 
             if ( PlayerService.ACTION_PLAY_PAUSE.equals(action) ) {
-                mIsPlaying = !mIsPlaying;
+                PlayerService.this.togglePlayer();
                 PlayerService.this.updateNotification();
             }
             else if ( PlayerService.ACTION_PREV.equals(action) ) {
-                mCurrentPosition = --mCurrentPosition < 0 ? mTracks.size() - 1 : mCurrentPosition;
-                PlayerService.this.updateNotification();
+                PlayerService.this.playPrev();
             }
             else if ( PlayerService.ACTION_NEXT.equals(action) ) {
-                mCurrentPosition = ++mCurrentPosition % mTracks.size();
-                PlayerService.this.updateNotification();
+                PlayerService.this.playNext();
             }
             else if ( PlayerService.ACTION_CLOSE.equals(action) ) {
                 PlayerService.this.stopForeground(true);
+                PlayerService.this.stopSelf();
             }
         }
     }
