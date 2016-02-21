03de1a50250d9fb0fcbd75ceb8777d29ba004e20
Kirill Bykov <bikovgta13@gmail.com>
2016-02-21 15:04:04 +0300

    Get data from intent in the service

--- app/src/main/java/org/kirillius/mymusic/PlayerService.java
+++ app/src/main/java/org/kirillius/mymusic/PlayerService.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.vk.sdk.api.model.VKApiAudio;
+
+import java.util.ArrayList;
+
 /**
  * Created by Kirill on 21.02.2016.
  */
@@ -14,9 +18,18 @@ public class PlayerService extends Service {
     public static final String EXTRA_POSITION = "ExtraPosition";
     public static final String EXTRA_TOTAL = "ExtraTotal";
 
+    private ArrayList<VKApiAudio> mTracks = new ArrayList<>();
+    private int mCurrentPosition = 0;
+    private int mTotalCount = 0;
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+
+        mTracks = intent.getParcelableArrayListExtra(EXTRA_TRACKS);
+        mCurrentPosition = intent.getIntExtra(EXTRA_POSITION, 0);
+        mTotalCount = intent.getIntExtra(EXTRA_TOTAL, 0);
+
         return START_NOT_STICKY;
     }
 
